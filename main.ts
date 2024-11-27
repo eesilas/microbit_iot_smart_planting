d96ebb65599bf8e1dd85ffbeb6653cc248e9414d
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-27 17:14:26 +0800

    Peristaltic pump smart planting IoT system

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    pins.analogWritePin(AnalogPin.P2, 250)
+})
+let humidity = 0
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_ledBlank(PIN.P15)
 microIoT.microIoT_setIndexColor(PIN.P15, 0, 4, 0xff0000)
 microIoT.microIoT_showUserText(0, "DFRobot")
 microIoT.microIoT_MQTT(
-"yourIotId",
-"yourIotPwd",
-"yourIotTopic",
-microIoT.SERVERS.China
+"9Vo9OLDRP2mZidOOpU8pBRy7",
+"EEuuaO2qa5XrUSV2nEsqe7CveujRovuN",
+"humidity/humidity",
+microIoT.SERVERS.English
 )
 basic.forever(function () {
-    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
-    basic.pause(500)
-    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
-    basic.pause(500)
-    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 180)
-    basic.pause(500)
+    humidity = pins.analogReadPin(AnalogReadWritePin.P1)
+    microIoT.microIoT_showUserText(0, "Humidity" + humidity)
+    microIoT.microIoT_SendMessage(convertToText(humidity), microIoT.TOPIC.topic_0)
 })
