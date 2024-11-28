6cef710b819f085a8cc320cca15fdd96a3bd9a26
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-28 09:59:32 +0800

    OLED display Welcome to Smart Home

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ input.onButtonPressed(Button.A, function () {
 let humidity = 0
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_ledBlank(PIN.P15)
-microIoT.microIoT_setIndexColor(PIN.P15, 0, 4, 0xff0000)
-microIoT.microIoT_showUserText(0, "DFRobot")
+microIoT.microIoT_setIndexColor(PIN.P15, 0, 2, 0xff0000)
+microIoT.microIoT_showUserText(0, "Welome to Smart Home!")
 microIoT.microIoT_MQTT(
 "9Vo9OLDRP2mZidOOpU8pBRy7",
 "EEuuaO2qa5XrUSV2nEsqe7CveujRovuN",
